8194744a5a469a47b7c46b47861f8722a217570b
pythops <contact@pythops.com>
2023-10-15 17:17:42 +0200

    init database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.database import db
+
+from app.config import config
 
 
 def init_app():
     api = FastAPI()
+    db.init(uri=config.DB_URI)
 
     from app.errors import error_handler
     from app.api import routes
