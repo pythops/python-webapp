c0c2033efde5a65c0e6deb86e85ceaf44d864726
pythops <contact@pythops.com>
2023-10-15 17:17:53 +0200

    update controllers

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -1,6 +1,10 @@
+from app.models import User
+
+
 async def create_new_user(username: str, email: str):
-    pass
+    await User.create(username=username, email=email)
 
 
 async def get_users() -> list:
-    return []
+    users = await User.get_all()
+    return [await user.to_dict() for user in users]
